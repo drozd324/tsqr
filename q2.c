84553e00a6248e5c649080f4e8d4f39a94692046
drozdtux <drozdp@gmail.com>
2025-02-06 20:08:20 +0000

    fucking hell allocating memory for a block matrix is a pain

--- q2.c
+++ q2.c
@@ -4,15 +4,16 @@
 #include <cblas.h>
 
 //row major ideology
+//change struct to struct pointers in functions pass
 
 struct _Matrix{
-	double* matrix;
+	double* mat;
 	int m;
 	int n;
 }matrix;
 
 struct _block_Matrix{
-	matrix* block_matrix;
+	matrix* block_mat;
 	int m;
 	int n;
 }block_matrix;
@@ -32,10 +33,7 @@ int main(){
 	}
 	
 	double* r = calloc(n * n, sizeof(double));
-	
-	qr_decomp(m, n, a, r);
 
-		
     return 0; 
 }
 
@@ -49,23 +47,25 @@ void print_matrix(int m, int n, double* a, int lda) {
     }
 }
 
-void qr_decomp(int m, int n, double* a, double* r){
+void qr_decomp(int m, int n, double* a, double* q, double* r){
     int     lda = n;
 	double* tau = malloc(fmin(m, n) * sizeof(double));
 	
+	memcpy(q, a, n*m * sizeof(double));
+	
 	// Double precision GEneral QR Factorisation
-    LAPACKE_dgeqrf(LAPACK_ROW_MAJOR, m, n, a, lda, tau); 
+    LAPACKE_dgeqrf(LAPACK_ROW_MAJOR, m, n, q, lda, tau); 
 	
 	// write to R to r
     for (int i=0; i<n; i++) {
         for (int j=i; j<n; j++){
-            r[i*n + j] = a[i*n + j];
+            r[i*n + j] = q[i*n + j];
 		}
 	}
 
 	int k = fmin(m, n);
 	// Double precision ORthogonal Generate? QR Factorisation
-   	LAPACKE_dorgqr(LAPACK_ROW_MAJOR, m, n, k, a, lda, tau); 
+   	LAPACKE_dorgqr(LAPACK_ROW_MAJOR, m, n, k, q, lda, tau); 
 }
 
 
@@ -82,39 +82,36 @@ void decomp1d(int n, int N, int block_index, int s, int e){
     }
 }
 
-void decomp_matrix(matrix* a, int M, int N, block_matrix* block_a){
-	int m = a.m;
-	int n = a.n;	
-	double* a = a.matrix;
-
-	block_a.m = M;
-	block_a.n = N;
-
+//void decomp_matrix(matrix* a, int M, int N, block_matrix* block_a){
+//block_a->m = M;
+//block_a->n = N;	
+//block_a->block_matrix = malloc(block->m * block->n * sizeof(matrix*));
+void decomp_matrix(matrix* a, block_matrix* block_a){
 	int s1;
 	int e1;
 	int s2;
 	int e2;
 
-	for (int i=0; i<M; i++){
-        decomp1d(a.m, M, i, s1, e1);
-		for (int j=0; j<N; j++){
-            decomp1d(a.n, N, j, s2, e2);
+	for (int i=0; i < (block_a->m); i++){
+        decomp1d(a.m, block_a->m, i, s1, e1);
+		for (int j=0; j < (block_a->n); j++){
+            decomp1d(a.n, block_a->n , j, s2, e2);
            
 			matrix block;
 			block.m = e1 - s1;
 			block.n = e2 - s2;
 			
-			block.matrix = malloc(block.m * block.n * sizeof(double)); 
+			block.mat = malloc((block.m) * (block.n) * sizeof(double)); 
 			// gotta free this memory yourself after 
 			// you're finished with the block matrix
 			for (int k=0; k < block.m; k++){
 				for (int l=0; l < block.n; l++){
-                    block.matrix[k*block.n + l] = a.matrix[(s1+k)*a.n + s2+l];
+                    (block.mat)[k*(block.n) + l] = (a->mat)[(s1+k)*(a->n) + s2+l];
 				}
 			}	
 
-			block_a.block_matrix[i*n + j] = &block
-
+			(block_a->block_mat)[i*n + j] = &block
+		
 		}
 	}
 }
@@ -122,25 +119,91 @@ void decomp_matrix(matrix* a, int M, int N, block_matrix* block_a){
 
 
 //void comp_matrix(int m, int n, double* a, int M, int N, double* a){
-void comp_matrix(block_matrix block_a, matrix a){
+void comp_matrix(block_matrix* block_a, matrix* a){
     int row_step = 0;
     int col_step = 0;
     int sub_rows = 0;
     int sub_cols = 0;
     
-	for (int i=0; i<M; i++){
-		for (int j=0; j<N; j++){
+	for (int i=0; i<block_a->m; i++){
+		for (int j=0; j<block_a->n; j++){
             
-			sub_rows = block_a.block_matrix[i*N + j].m;
-			sub_cols = block_a.block_matrix[i*N + j].n;
+			sub_rows = (block_a.block_mat[i*N + j])->m;
+			sub_cols = (block_a.block_mat[i*N + j])->n;
 
         	for (int k=0; k<sub_rows; k++){
         		for (int l=0; l<sub_rows; l++){
-                    a.matrix[(row_step + k)*n + (col_step + l)] = block[k*sub_cols + l];
-                    
+                    a.mat[(row_step + k)*n + (col_step + l)] = block_a[k*sub_cols + l];
+
+			// maybe
+			free(&(block_a.block_mat[i*N + j]));
+
             col_step += sub_cols;
         row_step += sub_rows;
         col_step = 0;
 	}
 }
 
+// calloc q.matrix
+void tsqr(matrix* a, matrix* q, matrix* r){
+	for (int i=0; i<m; i++){
+		q.mat[i*n + i] = 1;
+	}
+
+	int rows_num = 4;
+	block_matrix* A;
+    A->m = rows_num;
+    A->n = 1;
+	decomp_matrix(a, A);
+	
+	for (int iter=0; iter<3; iter++){
+		
+		block_matrix* Q_temp;
+        Q_temp->m = rows_num;
+        Q_temp->n = rows_num;
+		Q_temp->block_mat = malloc(rows_num * rows_num * sizeof(matrix*));
+		
+		block_matrix* R_temp;
+        R_temp->m = rows_num;
+        R_temp->n = 1;
+		R_temp->block_mat = malloc(rows_num * sizeof(matrix*));
+        
+        // allocate memory for Q_temp
+        for (int i=0; i<rows_num; i++){
+            for (int j=0; j<rows_num; j++){
+                // (array of matrix structs)[of matrix struct]
+                // (block matrix)[index of matrix]->(array of double representing matrix)  =  calloc size m_i*n array of doubles representing matrix
+                (Q_temp->block_mat)[i*(Q_temp->n) + j]->mat = calloc((A->block_mat->m) * (a->n), sizeof(double*));
+                (Q_temp->block_mat)[i*(Q_temp->n) + j]->m   = A->block_mat->m;
+                (Q_temp->block_mat)[i*(Q_temp->n) + j]->n   = a->n;
+            }
+        } 
+        
+        // allocate memory for R_temp
+        for (int i=0; i<rows_num; i++){
+            (R_temp->block_mat)[i]->mat = calloc((a->n) * (a->n), sizeof(double*));
+            (R_temp->block_mat)[i]->m   = A->block_mat->m;
+            (R_temp->block_mat)[i]->n   = a->n;
+
+        }
+		
+		for (int i=0; i<rows_num; i++){
+			qr_decomp(m, n, R->mat,
+                // index the block matrix and pick out the array of doubles representing matrix 
+                (Q_temp->block_mat)[i*(Q_temp->n) + i]->mat, 
+                (R_temp->block_mat)[i                ]->mat);
+		} 
+	
+        for (int i=0; i<rows_num; i++){
+            for (int j=0; j<rows_num; j++){
+                if (i != j){
+                    (Q_temp->block_mat)[i*(Q_temp->n) + j] 
+                    = calloc((Q_temp->m) * (Q_temp->n), sizeof(double)); 
+                }
+            }
+        }	
+
+	}
+	
+
+}
